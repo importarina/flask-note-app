c5401689f2fa556c4dfdf2818a911ace79cc4e6b
Arina Momajjed <arinamomajjed@gmail.com>
2021-09-30 19:55:06 -0400

    note per user website

--- website/views.py
+++ website/views.py
@@ -1,12 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 # views is a Blueprint of our app, meaning it has a roots/urls defined inside it
 # so we don't have all of our views in one file, and we can split them up into multiple
 # files in an organized way.
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required  # cannot get to the homepage unless logged in
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash("Note is too short.", category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note added!", category='success')
+    # then go back to the homepage and render that note
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({})
